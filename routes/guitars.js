3389583e39cd2ebe662b2e623af58f2b564b256b
KjetilHerheimJohannesen <kjetil.herheim.johannesen@gmail.com>
2024-11-12 21:35:22 +0100

    Test

--- routes/guitars.js
+++ routes/guitars.js
@@ -49,7 +49,9 @@ router.post("/", ensureAuth, jsonParser, async function (req, res, next) {
     res.status(200).json({ success: true, message: "Guitar added successfully.", newGuitar });
 });
 
-router.delete("/:id", async function (req, res, next) {
+router.delete("/", async function (req, res, next) {
+    const { id } = req.body;
+    console.log(id);
     res.status(200).json({ success: true, message: "Guitar deleted Successfully" });
 });
 
