e26ab378f486a1f971e1ce51ad14deace7063e58
KjetilHerheimJohannesen <kjetil.herheim.johannesen@gmail.com>
2024-11-12 20:44:48 +0100

    Tests something weird

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/auth", indexRouter);
+app.use("/", indexRouter);
 app.use("/guitars", guitarsRouter);
 app.use("/populate", populateRouter);
 
